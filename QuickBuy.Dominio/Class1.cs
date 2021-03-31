e8359bb8e03b57eb6d0ecc67cc37685aa71daa06
Mauricio Filho <mauriciocirinofilho@gmail.com>
2021-03-31 13:48:49 -0300

    Criacao de Entidades

--- QuickBuy.Dominio/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace QuickBuy.Dominio
-{
-    public class Class1
-    {
-        public string batata { get; set; }
-    }
-}
---
+++ QuickBuy.Dominio/OBjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,51 @@
+﻿using System.Dynamic;
+using QuickBuy.Dominio.Enumerados;
+
+namespace QuickBuy.Dominio.OBjetoDeValor
+{
+    public class FormaPagamento
+    {
+        private bool _ehBoleto;
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+            get { return Id == (int) TipoFormaPagamentoEnum.Boleto; }
+        }
+
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int) TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
