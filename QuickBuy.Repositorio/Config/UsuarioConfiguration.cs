a74d9c0cfac6e426509f0be0e206c6cb1bc3a330
Mauricio Filho <mauriciocirinofilho@gmail.com>
2021-04-08 18:22:24 -0300

    Commit DB

--- QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
+++ QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
@@ -31,6 +31,10 @@ namespace QuickBuy.Repositorio.Config
                 .IsRequired()
                 .HasMaxLength(50);
 
+            builder
+                .HasMany(u => u.Pedidos)
+                .WithOne(p => p.Usuario);
+
 
         }
     }
