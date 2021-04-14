4fcf26a09e88b69cb4ab6d52ac7b8dc9a80558db
Mauricio Filho <mauriciocirinofilho@gmail.com>
2021-04-14 16:58:13 -0300

    config api

--- QuickBuy.Repositorio/Repositorios/UsuarioRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/UsuarioRepositorio.cs
@@ -1,13 +1,14 @@
 ﻿using QuickBuy.Dominio.Contratos;
 using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
     public class UsuarioRepositorio : BaseRepositorio <Usuario>, IUsuariorepositorio
     {
-        public UsuarioRepositorio()
+        public UsuarioRepositorio(QuickBuyContexto quickBuyContexto) : base(quickBuyContexto)
         {
-            
+
         }
     }
 }
